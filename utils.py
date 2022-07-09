23ae0c17cd4c9ac14e247c3c2775be586ad06caa
Gleb Kushedow <kushedow@gmail.com>
2022-07-09 16:36:40 +0300

    docstring & typing added

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import random
 
 WORDS = ["alpha", "bravo", "charlie", "delta"]
 
-def get_random_word():
+def get_random_word() -> str:
+    """ Возвравщает случайно слово"""
     return random.choice(WORDS)
 
